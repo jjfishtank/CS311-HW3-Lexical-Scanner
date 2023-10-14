d13ef7cd6c8245d78d5a168c175d99c86dad0d84
jjfishtank <26157427+jjfishtank@users.noreply.github.com>
2023-10-13 17:53:55 -0700

    Fix main params. Add file open check.

--- src/LexicalScan.cpp
+++ src/LexicalScan.cpp
@@ -1,10 +1,14 @@
 ﻿#include "LexicalScanner.h"
 
-int main(char* filePath[]) {
+int main(int numArgs, char* filePath[]) {
 	ifstream ifile(filePath[1]);
-	SourceCodeScanner scanner(" \t\n");
+	if (!ifile) {
+		std::cerr << "Failed to open file: " << filePath[1] << '\n';
+		return 1;
+	}
+	SourceCodeScanner scanner(" \t\n()[]+-=,;");
 	scanner.scan(ifile);
-	scanner.printSummary();
+	cout << scanner.summary();
 
 	return 0;
 }
